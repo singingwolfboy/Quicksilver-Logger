aa98ed9af4809d142a4a99cb01d9ab3bfda47d4e
David Baumgold <baumgold@brandeis.edu>
2011-05-09 18:56:52 -0400

    Initial commit

---
+++ QSLoggerPlugIn.h
@@ -0,0 +1,18 @@
+//
+//  QSLoggerPlugIn.h
+//  QSLoggerPlugIn
+//
+//  Created by David Baumgold on 2011-05-09
+//
+
+#import <QSCore/QSObject.h>
+#import "QSLoggerPlugIn.h"
+#import <QSCore/QSParser.h>
+
+#define kQSLoggerPlugInType @"QSLoggerPlugInType"
+
+@interface QSLoggerPlugIn : NSObject
+{
+}
+@end
+
